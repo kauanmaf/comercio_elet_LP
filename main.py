667b8c1eaa8cfb2e936893c80b1f2acd40e76216
Larissa Lemos Afonso <126449248+LarissaLAfonso@users.noreply.github.com>
2023-10-30 15:40:25 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+from inventario import Vendas, Inventario
+
+compras = True
+
+while compras:
+    cliente = input("Bem vindo à nossa loja!\n Que ação você quer realizar? (escolha entre 'repor', 'devolver' ou 'comprar').")
+
+    tipo = input("Que tipo de produto?").lower()
+    quantidade = input("Quantas unidades?").lower()
+
+    if cliente.lower() == "repor":
+        Vendas.reposicao(tipo, quantidade)
+    elif cliente.lower() == "comprar":
+        vendas = Vendas(quantidade)
+        Vendas.venda(tipo, quantidade)
+    elif cliente.lower() == "devolver":
+        quebrado = input("O produto está quebrado? Se sim, digite 's'. Caso contrário digite 'n'.")
+        if quebrado.lower() == "s":
+            Vendas.retorno(Vendas, tipo=tipo, qtd=quantidade, quebrado=True)
+        elif quebrado.lower() == "n":
+            Vendas.retorno(Vendas, tipo=tipo, qtd=quantidade, quebrado=False)
+        
+    continuar = input("Deseja fazer algo mais? Se sim, digite 's'. Caso contrário digite 'n'.")
+
+    if continuar.lower() == "n":
+        compras = False
+
+print("Foi um prazer te ter conosco!\n Volte sempre que precisar de um carro, de uma moto ou de uma bicicleta :3")    
+    
