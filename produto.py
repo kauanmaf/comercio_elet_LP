df829f6da94d154cc1e4ec977abad43748310a04
Larissa Lemos Afonso <126449248+LarissaLAfonso@users.noreply.github.com>
2023-10-30 21:49:23 -0300

    adicionei lista de marcas

--- produto.py
+++ produto.py
@@ -1,4 +1,9 @@
 from exception_comercio import erro_sem_estoque
+from random import choice
+
+marcas_bicicleta = ["Caloi", "Focus", "Giant"]
+marcas_carro = ["Ford", "Fiat", "Toyota", "Volkswagen"]
+marcas_moto = ["Honda", "Yamaha", "Suzuki"]
 
 # Classe criada para realizar a contagem de produtos em estoque
 class Produto:
@@ -17,13 +22,16 @@ class Carro(Produto):
     def __init__(self):
         super().__init__()
         self.codigo = "C" + str(Produto.codigo)
+        self.marca = choice(marcas_carro)
 
 class Moto(Produto):
     def __init__(self):
         super().__init__()
         self.codigo = "M" + str(Produto.codigo)
+        self.marca = choice(marcas_moto)
 
 class Bicicleta(Produto):
     def __init__(self):
         super().__init__()
         self.codigo = "B" + str(Produto.codigo)
+        self.marca = choice(marcas_bicicleta)
